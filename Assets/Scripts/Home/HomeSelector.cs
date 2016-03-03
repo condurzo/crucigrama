9efaf98ebc03976924b01125bb880edff8d2b8c2
Condurzo <maximiliano.condurzo@gmail.com>
2016-03-03 10:46:33 -0300

    Proyecto Creado - Pantalla Home (Seccion Cartas)

---
+++ Assets/Scripts/Home/HomeSelector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HomeSelector : MonoBehaviour {
+
+	public GameObject SeccionCrucigramas;
+	public GameObject SeccionCartas;
+
+	void SelectCrucigramas () {
+		SeccionCrucigramas.SetActive(true);
+		SeccionCartas.SetActive(false);
+	}
+	
+	void SelectCartas () {
+		SeccionCrucigramas.SetActive(false);
+		SeccionCartas.SetActive(true);
+	}
+
+
+
+
+}
