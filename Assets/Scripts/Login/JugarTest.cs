318895fc8bbefce45a61dcacd40793665edb7432
condurzo <maximiliano.condurzo@gmail.com>
2016-03-04 22:11:18 -0300

    Borrado Home Viejo
    Borrado Home viejo

---
+++ Assets/Scripts/Login/JugarTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JugarTest : MonoBehaviour {
+
+	// Use this for initialization
+	public void TestPlay () {
+        Application.LoadLevel("Home");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
