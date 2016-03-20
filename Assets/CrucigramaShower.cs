d94100843753e50c6c7f88a792d3da9b48e7049e
tejerolucas <tejerolucas@gmail.com>
2016-03-20 20:43:26 -0300

    Muestra Frases y Palabras correspondiente al crucigrama que elegiste

--- Assets/CrucigramaShower.cs
+++ Assets/CrucigramaShower.cs
@@ -3,12 +3,22 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class CrucigramaShower : MonoBehaviour {
+	public Button boton;
 	public Text Nombre;
 	public Text palabras;
 	public Text estado;
 	public int id;
 
+	void Awake(){
+		boton.onClick.AddListener(delegate() { Prender(); });
+	}
+
 	public void Resolver(){
 		Managerhome.instance.CargarCruci (id);
+	}
+
+	public void Prender(){
+		GameObject go = Managerhome.instance.crucigramajuego;
+		go.SetActive (true);
 	}
 }
--- Assets/Managerhome.cs
+++ Assets/Managerhome.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System;
 
 public class Managerhome : MonoBehaviour {
 	public static Managerhome instance;
 	public Text frasetext;
+	public Text palabrastext;
 	private Crucigrama Cruciactual;
 	private List<string> frases=new List<string>();
 	private int index;
+	public GameObject crucigramajuego;
 	// Use this for initialization
 	void Awake (){
 		index = 0;
@@ -22,11 +25,19 @@ public class Managerhome : MonoBehaviour {
 	}
 
 	public void CargarCruci(int id){
+		Debug.Log ("cargo cruci");
 		Cruciactual = Parser.instance.GetCrossword (id);
 		frases.Clear ();
 		for (int i = 0; i < Cruciactual.palabras.Length; i++) {
 			frases.Add (Cruciactual.palabras [i].def);
 		}
+		index = 0;
+		frasetext.text = frases [index];
+		string pal = "Palabras: ";
+		for (int i = 0; i < Cruciactual.palabras.Length; i++) {
+			pal += ";" + Cruciactual.palabras [i].nom;
+		}
+		palabrastext.text = pal;
 	}
 
 	public void Siguiente(){
