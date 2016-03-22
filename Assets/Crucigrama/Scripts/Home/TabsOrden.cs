1d0bf88f036bde5361ae5190ed0f7d785725eb18
Condurzo <maximiliano.condurzo@gmail.com>
2016-03-22 18:06:38 -0300

    Home - New Tab Bar - Borrado Materials - Seccion Encuestas.

--- Assets/Crucigrama/Scripts/Home/TabsOrden.cs
+++ Assets/Crucigrama/Scripts/Home/TabsOrden.cs
@@ -8,27 +8,27 @@ public class TabsOrden : MonoBehaviour {
 	public Image FeedBackBtn;
 
 	public Sprite Barra_Superior_Cartas_CERCA;
+	public Sprite Barra_Superior_Cartas_LEJOS;
 	public Sprite Barra_Superior_Cartas_ON;
 	public Sprite Barra_Superior_Crucigrama_CERCA;
 	public Sprite Barra_Superior_Crucigrama_LEJOS;
 	public Sprite Barra_Superior_Crucigrama_ON;
 	public Sprite Barra_Superior_Feedback_CERCA;
-	public Sprite Barra_Superior_Feedback_LEJOS;
 	public Sprite Barra_Superior_Feedback_ON;
 
 
 	public void EnCrucigrama () {
 		CrucigramaBtn.sprite = Barra_Superior_Crucigrama_ON;
-		CartasBtn.sprite = Barra_Superior_Cartas_CERCA;
-		FeedBackBtn.sprite = Barra_Superior_Feedback_LEJOS;
+		CartasBtn.sprite = Barra_Superior_Cartas_LEJOS;
+		FeedBackBtn.sprite = Barra_Superior_Feedback_CERCA;
 	}
 	public void EnCartas () {
-		CrucigramaBtn.sprite = Barra_Superior_Crucigrama_CERCA;
+		CrucigramaBtn.sprite = Barra_Superior_Crucigrama_LEJOS;
 		CartasBtn.sprite = Barra_Superior_Cartas_ON;
 		FeedBackBtn.sprite = Barra_Superior_Feedback_CERCA;
 	}
 	public void EnFeedBack () {
-		CrucigramaBtn.sprite = Barra_Superior_Crucigrama_LEJOS;
+		CrucigramaBtn.sprite = Barra_Superior_Crucigrama_CERCA;
 		CartasBtn.sprite = Barra_Superior_Cartas_CERCA;
 		FeedBackBtn.sprite = Barra_Superior_Feedback_ON;
 	}
